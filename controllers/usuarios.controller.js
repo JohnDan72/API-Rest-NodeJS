0c703d1cf174d7b58fe20a8973e679c6b36a0c03
Juan Dan <mr._dany@hotmail.com>
2021-08-04 11:54:49 -0500

    ultimos cambios en REST API 1

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,39 +1,39 @@
 const { response, request } = require('express');
 
 const usuariosGet = (req = request, res = response) => {
-    const query = req.query;
-    const {nombre, otra_edad} = req.query;
+  const query = req.query;
+  const { nombre, otra_edad } = req.query;
 
-    res.status(201).json({
-      msg: "get",
-      body: query,
-      nombre,
-      otra_edad
-    });
-  }
+  res.status(201).json({
+    msg: "get",
+    body: query,
+    nombre,
+    otra_edad
+  });
+}
 
-  const usuariosPost = (req = request, res = response) => {
-    const {nombre, edad} = req.body;
-    
-    res.status(201).json({
-      msg: 'put',
-      body: req.body,
-      edad: edad,
-      nombre: nombre,
-      
-    });
-  }
-  const usuariosPut = (req = request, res = response) => {
-    res.status(201).json({
-      msg: "put",
-      body: req.params
-    });
-  }
-  const usuariosDelete = (req = request, res = response) => {
-    res.status(201).json({
-      msg: "delete",
-    });
-  }
+const usuariosPost = (req = request, res = response) => {
+  const { nombre, edad } = req.body;
+
+  res.status(201).json({
+    msg: 'post',
+    body: req.body,
+    edad: edad,
+    nombre: nombre,
+
+  });
+}
+const usuariosPut = (req = request, res = response) => {
+  res.status(201).json({
+    msg: "put",
+    body: req.params
+  });
+}
+const usuariosDelete = (req = request, res = response) => {
+  res.status(201).json({
+    msg: "delete",
+  });
+}
 
 
 module.exports = {
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete } = require('../controllers/usuarios.controller');
 
-const router = Router();
-      router.get("/", usuariosGet);
-      router.post("/", usuariosPost);
-      router.put("/:id_user", usuariosPut);
-      router.delete("/", usuariosDelete);
+const   router = Router();
+        router.get("/", usuariosGet);
+        router.post("/", usuariosPost);
+        router.put("/:id_user/:id_otro", usuariosPut);
+        router.delete("/", usuariosDelete);
 
 module.exports = {
     router
